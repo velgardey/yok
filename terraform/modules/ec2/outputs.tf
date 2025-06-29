ffb81baba4c7f10127e6adfc8fded92e2d7a8af8
velgardey <arch.velgardey@gmail.com>
2025-06-30 02:27:10 +0530

    minor github actions fix

--- terraform/modules/ec2/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_eip.web_server.public_ip
-}
-
-output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.web_server.id
-}
